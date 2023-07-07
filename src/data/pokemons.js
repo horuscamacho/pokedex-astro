eff45e56b213127d5efd6fd4e1adaceac4a6742d
Horus Sinhue Camacho <62359674+horuscamacho@users.noreply.github.com>
2023-07-06 22:03:40 -0600

    Terminando maquetado de las dos páginas principales del proyecto

---
+++ src/data/pokemons.js
@@ -0,0 +1,30 @@
+export const pokemons = [
+	{
+		id: 'pikachu',
+		name: 'Pikachu',
+		description: 'Pikachu es un Pokémon de tipo eléctrico introducido en la primera generación. Es el Pokémon más conocido de la historia, mayormente por ser el acompañante del protagonista del anime, Ash Ketchum y la mascota representante de la franquicia Pokémon.',
+		img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/25.svg",
+		type: "eléctrico"
+	},
+	{
+		id: 'squirtle',
+		name: 'Squirtle',
+		description: 'Squirtle (ゼニガメ Zenigame en japonés) es un Pokémon de tipo agua introducido en la primera generación. Es uno de los Pokémon iniciales en la región Kanto, junto a Bulbasaur y Charmander, en las ediciones Pokémon Rojo, Pokémon Verde y Pokémon Azul y Pokémon Rojo Fuego y Pokémon Verde Hoja.',
+		img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/shiny/7.png",
+		type: "agua"
+	},
+	{
+		id: 'charmander',
+		name: 'Charmander',
+		description: 'Charmander es un pequeño lagarto bípedo. Sus características de fuego son resaltadas por su color de piel anaranjado y su cola, cuya punta está envuelta en llamas. Charmander y sus evoluciones, Charmeleon y Charizard, tienen una pequeña llama en la punta de sus colas desde que nacen.',
+		img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+		type: "fuego"
+	},
+	{
+		id: 'meowth',
+		name: 'Meowth',
+		description: 'Meowth tiene el diseño de un gato, con pelaje color crema, y ​​tiene las patas y la punta de la cola marrones. Tiene bigotes, orejas negras y marrones y una moneda de oro en la cabeza. Meowth pasa la mayor parte del día durmiendo y camina por las calles de la ciudad por la noche.\n',
+		img: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/52.svg",
+		type: "normal"
+	},
+]
